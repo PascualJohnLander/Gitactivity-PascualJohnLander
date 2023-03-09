e598dc9dab3c288cb6d138be9c3ec31ddffe29a3
PascualJohnLander <pascualjohnlander@gmail.com>
2023-03-09 22:51:13 +0800

    My Educational Background added

--- git-python-pascual.py
+++ git-python-pascual.py
@@ -8,3 +8,7 @@ Created on Thu Mar  9 22:04:40 2023
 print("John Lander M. Pascual")
 print("Balanacan, Mogpog, Marinduque")
 print("July 28, 2003")
+#MyEducationalBackground
+print("College of Information and Computing Sciences")
+print("Bachelor of Science in Information Technology")
+print("2B")
