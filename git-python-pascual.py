aab4b66b91d0132527e6589d6c6615863401b731
PascualJohnLander <pascualjohnlander@gmail.com>
2023-03-09 22:15:09 +0800

    My Personal Info Added

---
+++ git-python-pascual.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  9 22:04:40 2023
+
+@author: johnlander
+"""
+
+print("John Lander M. Pascual")
+print("Balanacan, Mogpog, Marinduque")
+print("July 28, 2003")
